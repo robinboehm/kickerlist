1da94a96f48595aa52599a40b16a8c44cb56c772
Robin Böhm <robin.boehm@symetics.com>
2017-09-16 18:16:19 +0200

    add @Input for waitingTeams to <team-list> component

--- src/app/team-list/team-list.component.ts
+++ src/app/team-list/team-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {Team} from "../team";
 
 @Component({
   selector: 'team-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team-list.component.css']
 })
 export class TeamListComponent implements OnInit {
+
+  @Input() waitingTeams: Team[];
 
   constructor() { }
 
