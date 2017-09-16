88478aa61e45d0cf935e09d9fa38350dd683604c
Robin Böhm <robin.boehm@symetics.com>
2017-09-16 18:17:00 +0200

    use GameService in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {GameService} from "./game/game.service";
 
 @Component({
   selector: 'kickertable-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = '';
+
+  constructor(public game: GameService){}
+
 }
